676c30df543c0a4f91eb8819876f9717cc9dc33e
BenJarrett <b.cl.jarrett@gmail.com>
2021-08-12 13:18:36 -0500

    Urban Planner

--- UrbanPlanner/Building.cs
+++ UrbanPlanner/Building.cs
@@ -9,19 +9,44 @@ namespace UrbanPlanner
     class Building
     {
 
-        public int Stories { get; set; }
-        public double Width { get; set; }
-        public double Depth { get; set; }
+        public int Stories { get; set; } = 4;  // REMINDER: A get property accessor is used to return the property value, and a set property accessor is used to assign a new value.
+        public double Width { get; set; } = 20.4;
+        public double Height { get; set; } = 40.5;
         public double Volume
         {
-            get => Width * Depth * (3 * Stories);
+            get => Width * Height * (3 * Stories);
         }
 
-        string _designer = "Ben";
-        string _Address;
-        public Building(string address)
+        //Private Fields
+        // Fields typically store the data that must be accessible to more than one class method and must be stored for longer than the lifetime of any single method.
+        // an underscore denotes private fields.
+        private string _designer = "Ben Jarrett";
+        private DateTime _constructionDate = DateTime.Now;
+        private string _address;
+        private string _owner;
+
+        public Building(string address) // REMINDER: Constructors set default values & limit instantiation
         {
-            Address = address;
+            _address = address;
+        }
+
+        public void Construct()
+        {
+            _constructionDate = DateTime.Now;
+        }
+
+        public void Purchase(string purchaser)
+        {
+            _owner = purchaser;
+        }
+
+        public void Display()
+        {   
+            Console.WriteLine($"{ _address} --------------");
+            Console.WriteLine($"Designed by {_designer}");
+            Console.WriteLine($"Constructed on {_constructionDate}");
+            Console.WriteLine($"Owned by {_owner}");
+            Console.WriteLine($"{Volume} cubic meters of space");
         }
 
     }
--- UrbanPlanner/Program.cs
+++ UrbanPlanner/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace UrbanPlanner
 {
@@ -6,7 +7,35 @@ namespace UrbanPlanner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Building FiveOneTwoEigth = new Building("512 8th Avenue");
+            
+                FiveOneTwoEigth.Width = 50.9;
+                FiveOneTwoEigth.Height = 35.1;
+                FiveOneTwoEigth.Stories = 1;
+                FiveOneTwoEigth.Construct();
+                FiveOneTwoEigth.Purchase("Richard");
+                FiveOneTwoEigth.Display();
+
+
+            Building One = new Building("123 Wake Drive");
+                One.Width = 60.3;
+                One.Height = 40.2;
+                One.Stories = 6;
+                One.Construct();
+                One.Purchase("Becky");
+                One.Display();
+
+
+            Building Three = new Building("123 Wake Drive");
+                One.Width = 90.5;
+                One.Height = 57.2;
+                One.Stories = 5;
+                One.Construct();
+                One.Purchase("Bob");
+                One.Display();
+
+
         }
     }
 }
