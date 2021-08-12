308a8de6f901cb0a7cf8580d267db552e6c04f4f
BenJarrett <b.cl.jarrett@gmail.com>
2021-08-12 12:03:14 -0500

    Add project files.

---
+++ UrbanPlanner/Building.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrbanPlanner
+{
+    class Building
+    {
+
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Volume
+        {
+            get => Width * Depth * (3 * Stories);
+        }
+
+        string _designer = "Ben";
+        string _Address;
+        public Building(string address)
+        {
+            Address = address;
+        }
+
+    }
+}
